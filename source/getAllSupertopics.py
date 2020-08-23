06673e37475b483c8bdc4f6ce6db046c44ba8a4e
Zhifei <zj98@cornell.edu>
2020-08-22 20:32:25 -0400

    save

--- source/getAllSupertopics.py
+++ source/getAllSupertopics.py
@@ -4,7 +4,7 @@
 """
 import tools.login as login
 import tools.scrollNclick as browser
-import csv
+import time
 #import sys
 # sys.path.append('../')
 url = "https://huati.weibo.cn/discovery/super?suda"
@@ -12,22 +12,21 @@ topics = ["明星", "饭圈", "CP", "游戏", "动漫", "综艺", "电视剧", "
           "互联网", "好好学习", "艺术", "设计美学", "舞蹈", "旅游", "校园", "搞笑幽默", "行业", "财经", "家居", "育儿", "医疗", "养生", "数码", "汽车", "科普", "军事", "历史", "宗教", "收藏", "房产", "航空"]
 
 
-def testLogIn():
-    session = login.LogIn()
-    session.main()
-
-
 def testbrowser():
     #session = login.LogIn()
     # session.main()
-    browser.init()
+    browser.init("https://huati.weibo.cn/discovery/super?suda")
+    info = browser.getAll
+    browser.saveCSV(info)
     #result = browser.isPresent()
     #print('判断页面1成功 0失败  结果是=%d' % result)
 
 
 def main():
-    testbrowser()
+    browser.repostSpider()
 
 
 if __name__ == '__main__':
+    #start = time.clock()
     main()
+    print("花费时长为%d" % time.process_time)
